9417c5b690123f2146ee5f34fe57e4a775ec1497
WheelzMonster <35692774+WheelzMonster@users.noreply.github.com>
2018-06-25 00:09:14 +0200

    changes
    changed the js file over maxim's, and began styling a header

--- main.js
+++ main.js
@@ -1,116 +1,127 @@
 var minutes;
 var secondes;
 var participants;
-var currentId;
+var currentId = 0;
 var isTimerActive;
 var isStart = true;
 
-$.getJSON("./config.json", function (data) {
-    minutes = data.chrono.minutes;
-    secondes = data.chrono.secondes;
-    participants = data.participants;
+// Write the name of the current participant
+function writeCurrentName() {
+  $('h1').text(participants[currentId].nom);
+}
 
+// Write the time into a timer
+function showTimer() {
+  var showMinutes;
+  var showSecondes;
+
+  if (secondes <= 0 && minutes <= 0) {
+    $('#timer').text("C'est fini !");
+  } else {
     if (minutes < 10) {
-        showMinutes = "0" + minutes;
+      showMinutes = '0' + minutes;
     } else {
-        showMinutes = minutes;
+      showMinutes = minutes;
     }
 
     if (secondes < 10) {
-        showSecondes = "0" + secondes;
+      showSecondes = '0' + secondes;
     } else {
-        showSecondes = secondes;
+      showSecondes = secondes;
     }
 
-    $("#timer").text(showMinutes + ":" + showSecondes);
-
-    showParticipants();
-});
-
-function writeCurrentName() {
-    $("h1").text(participants[currentId].nom)
+    $('#timer').text(showMinutes + ':' + showSecondes);
+  }
 }
 
-function showParticipants() {
-    currentId = 0;
+// Load the JSON data
+$.getJSON('./config.json', function(data) {
+  minutes = data.chrono.minutes;
+  secondes = data.chrono.secondes;
+  participants = data.participants;
 
-    writeCurrentName();
+  for (
+    var participantIndex = 0;
+    participantIndex < participants.length;
+    participantIndex++
+  ) {
+    var participantId = 'participant' + participantIndex;
 
-    for (var participantIndex = 0; participantIndex < participants.length; participantIndex++) {
-        var participantId = "participant" + participantIndex;
-        
-        if (participantIndex === currentId) {
-            $("ul").append("<li id='" + participantId + "' class='active'>" + participants[participantIndex].nom + "</li>");
-        } else {
-            $("ul").append("<li id='" + participantId + "'>" + participants[participantIndex].nom + "</li>");
-        }
-    }
-};
-
-function getParticipantId() {
-    return "#participant" + currentId;
-};
-
-$("#suivant").click(function() {
-    if(currentId < participants.length - 1){
-        $(getParticipantId()).removeClass("active");
-        currentId++;
-        $(getParticipantId()).addClass("active");
-        writeCurrentName();
-    }
-});
-
-$("#precedent").click(function() {
-    if(currentId > 0) {
-        $(getParticipantId()).removeClass("active");
-        currentId--;
-        $(getParticipantId()).addClass("active");
-        writeCurrentName();
-    }
-});
-
-function timer() {
-    var showMinutes;
-    var showSecondes;
-
-    if (isTimerActive == true) {
-        if (secondes <= 0 && minutes <= 0) {
-            $("#timer").text("C'est fini !");
-        } else {
-            if (minutes < 10) {
-                showMinutes = "0" + minutes;
-            } else {
-                showMinutes = minutes;
-            }
-
-            if (secondes < 10) {
-                showSecondes = "0" + secondes;
-            } else {
-                showSecondes = secondes;
-            }
-
-            $("#timer").text(showMinutes + ":" + showSecondes);
-            secondes = secondes - 1;
-            setTimeout(timer, 1000);
-
-            if (secondes < 0) {
-                secondes = 59;
-                minutes = minutes - 1;
-            }
-        }
-    }     
-};
-
-$("#switch").click(function() {
-    if(isStart == true) {
-        isTimerActive = true;
-        isStart = false;
-        $("#switch").text("Stop");
+    if (participantIndex === currentId) {
+      $('ul').append(
+        "<li id='" +
+          participantId +
+          "' class='active'>" +
+          participants[participantIndex].nom +
+          '</li>',
+      );
     } else {
-        isTimerActive = false;
-        isStart = true;
-        $("#switch").text("Start");
+      $('ul').append(
+        "<li id='" +
+          participantId +
+          "'>" +
+          participants[participantIndex].nom +
+          '</li>',
+      );
     }
+  }
 
-    timer();
+  writeCurrentName();
+  showTimer();
+
+  $('li').click(function() {
+    console.log('test');
+  });
+});
+
+// Get the current participant ID
+function getParticipantId() {
+  return '#participant' + currentId;
+}
+
+$('#suivant').click(function() {
+  if (currentId < participants.length - 1) {
+    $(getParticipantId()).removeClass('active');
+    currentId++;
+    $(getParticipantId()).addClass('active');
+    writeCurrentName();
+  }
+});
+
+$('#precedent').click(function() {
+  if (currentId > 0) {
+    $(getParticipantId()).removeClass('active');
+    currentId--;
+    $(getParticipantId()).addClass('active');
+    writeCurrentName();
+  }
+});
+
+// Set the timer every secondes
+function timer() {
+  if (isTimerActive == true) {
+    showTimer();
+
+    secondes = secondes - 1;
+    setTimeout(timer, 1000);
+
+    if (secondes < 0) {
+      secondes = 59;
+      minutes = minutes - 1;
+    }
+  }
+}
+
+$('#switch').click(function() {
+  if (isStart == true) {
+    isTimerActive = true;
+    isStart = false;
+    $('#switch').text('Stop');
+  } else {
+    isTimerActive = false;
+    isStart = true;
+    $('#switch').text('Start');
+  }
+
+  timer();
 });
