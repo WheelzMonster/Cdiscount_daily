d5802a2bfbf88f9095a9f4bb1a7f58a518585b3a
WheelzMonster <35692774+WheelzMonster@users.noreply.github.com>
2018-06-25 16:31:56 +0200

    timer pour temps de parole

--- main.js
+++ main.js
@@ -4,65 +4,62 @@ var participants;
 var currentId = 0;
 var isTimerActive;
 var isStart = true;
+var participantsTimer;
 
 // Write the name of the current participant
 function writeCurrentName() {
   $('h1').text(participants[currentId].nom);
 }
 
-// Write the time into a timer
-function showTimer() {
+function getTimer(minutes, secondes) {
   var showMinutes;
   var showSecondes;
 
+  if (minutes < 10) {
+    showMinutes = '0' + minutes;
+  } else {
+    showMinutes = minutes;
+  }
+
+  if (secondes < 10) {
+    showSecondes = '0' + secondes;
+  } else {
+    showSecondes = secondes;
+  }
+
+  return showMinutes + ':' + showSecondes;
+}
+
+// Write the time into a timer
+function showTimer() {
   if (secondes <= 0 && minutes <= 0) {
     $('#timer').text("C'est fini !");
   } else {
-    if (minutes < 10) {
-      showMinutes = '0' + minutes;
-    } else {
-      showMinutes = minutes;
-    }
-
-    if (secondes < 10) {
-      showSecondes = '0' + secondes;
-    } else {
-      showSecondes = secondes;
-    }
-
-    $('#timer').text(showMinutes + ':' + showSecondes);
+    $('#timer').text(getTimer(minutes, secondes));
   }
 }
 
 // Load the JSON data
 $.getJSON('./config.json', function(data) {
-  minutes = data.chrono.minutes;
-  secondes = data.chrono.secondes;
   participants = data.participants;
+  participantsTimer = [];
 
-  for (
-    var participantIndex = 0;
-    participantIndex < participants.length;
-    participantIndex++
-  ) {
+  minutes = participants.length * data.chrono.minutes;
+  secondes = participants.length * data.chrono.secondes;
+
+  while (secondes >= 60) {
+    minutes++;
+    secondes = secondes - 60;
+  };
+
+  for (var participantIndex = 0; participantIndex < participants.length; participantIndex++) {
     var participantId = 'participant' + participantIndex;
+    participantsTimer.push(0);
 
     if (participantIndex === currentId) {
-      $('ul').append(
-        "<li id='" +
-          participantId +
-          "' class='active'>" +
-          participants[participantIndex].nom +
-          '</li>',
-      );
+      $('ul').append("<li id='" + participantId + "' class='active'>" + participants[participantIndex].nom + '<span>00:00</span></li>');
     } else {
-      $('ul').append(
-        "<li id='" +
-          participantId +
-          "'>" +
-          participants[participantIndex].nom +
-          '</li>',
-      );
+      $('ul').append("<li id='" + participantId + "'>" + participants[participantIndex].nom + '<span>00:00</span></li>');
     }
   }
 
@@ -70,7 +67,12 @@ $.getJSON('./config.json', function(data) {
   showTimer();
 
   $('li').click(function() {
-    console.log('test');
+    var participantId = $(this).attr('id');
+    $(getParticipantId()).removeClass('active');
+    $(this).addClass('active');
+    currentId = participantId.substr(participantId.length - 1);
+
+    writeCurrentName();
   });
 });
 
@@ -101,7 +103,8 @@ $('#precedent').click(function() {
 function timer() {
   if (isTimerActive == true) {
     showTimer();
-
+    participantsTimer[currentId]++;
+    $(".active span").text(participantsTimer[currentId]);
     secondes = secondes - 1;
     setTimeout(timer, 1000);
 
